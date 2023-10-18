3018e844166cee4380dd18d5be493a6875fdef48
itzTedx <zomboi.offi@gmail.com>
2023-10-18 18:50:22 +0400

    First Commit

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <div className="text-center text-xs text-black">
+          &copy; Something URL, Inc. All rights Reserved
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/ui/container.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return <div className="mx-auto max-2-7xl">{children}</div>;
+};
+
+export default Container;
