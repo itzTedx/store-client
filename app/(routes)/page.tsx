4c326c86fa87f9490c872def09c6113c130604f2
itzTedx <zomboi.offi@gmail.com>
2023-10-24 11:55:24 +0400

    Added Description

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,27 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import Billboard from '@/components/Billboard'
-import Container from '@/components/ui/container'
-import getBillboard from '@/actions/get-billboard'
-import getProducts from '@/actions/get-products'
-import ProductList from '@/components/ProductList'
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/ProductList";
 
-export const revalidate = 0
+export const revalidate = 0;
 
 const HomePage = async () => {
-  const billboard = await getBillboard('90f88b2f-7192-4d66-ae65-9fc4170773d9')
-  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("90f88b2f-7192-4d66-ae65-9fc4170773d9");
+  const products = await getProducts({ isFeatured: true });
 
   return (
     <Container>
-      <div className='space-y-10 pb-10'>
+      <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className='flex flex-col gap-y-8 sm:px-6 lg:px-8 px-4'>
-        <ProductList title='Featured Products' items={products} />
+        <div className="flex flex-col gap-y-8 sm:px-6 lg:px-8 px-4">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
