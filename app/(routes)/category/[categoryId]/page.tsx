977c08a5b831d4a9770c7a8ff5ed5dc3aa9aa5d9
itzTedx <zomboi.offi@gmail.com>
2023-10-24 19:53:04 +0400

    category

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import getCategory from "@/actions/get-category";
+import getColors from "@/actions/get-colors";
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const categories = await getCategory(params.categoryId);
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={categories.billboard} />
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
