7fbf21254c7185abaff13f5becf1d09b0200e8b3
melwin-tedd <musictrendiez@gmail.com>
2023-10-30 01:31:49 +0400

    Billboard UI Update

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,18 +1,49 @@
-import { Billboard as BillboardType } from "@/types";
-import Image from "next/image";
+import { Billboard as BillboardType } from "@/types"
+import Image from "next/image"
+import { Gloock } from "next/font/google"
+import Link from "next/link"
+import Button from "./ui/button"
+import { cn } from "@/lib/utils"
+import { ArrowUpRight } from "lucide-react"
 
 interface BillboardProps {
-  data: BillboardType;
+  data: BillboardType
 }
+
+const gloock = Gloock({ subsets: ["latin"], weight: "400" })
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl flex flex-col-reverse md:grid md:grid-cols-2 overflow-hidden gap-4">
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl flex flex-col-reverse md:grid md:grid-cols-2 overflow-hidden gap-11">
       <div className="h-full w-full flex flex-col justify-center gap-y-8">
-        <div className="block font-bold text-3xl sm:text-5xl sm:max-w-xl max-w-xs">
+        <h1
+          className={`${gloock.className} block font-bold text-4xl sm:text-6xl sm:max-w-xl `}
+        >
           {data?.label}
+        </h1>
+
+        <p className="text-xl sm:text-3xl tracking-tight leading-relaxed">
+          {data?.description}
+        </p>
+        <div className="flex justify-between">
+          <Link
+            href="/products"
+            className="px-6 py-3 bg-lime-500 shadow-[0_0_0_1.5px_#000] border-black rounded-full font-bold hover:shadow-[0_0_0_1.5px_#000,0_7px_0_1.5px_#000] hover:translate-y-[-10px] transition-bounceBtn "
+          >
+            Order Now
+          </Link>
+          <Link
+            href="/"
+            className="group flex items-center gap-4 text-right font-medium"
+          >
+            Browse
+            <br />
+            Catalogue
+            <span className="rounded-full border border-black p-3 group-hover:bg-lime-100">
+              <ArrowUpRight />
+            </span>
+          </Link>
         </div>
-        {data?.description}
       </div>
       <div className="rounded-[53px] relative aspect-square overflow-hidden !bg-cover ">
         <Image
@@ -23,7 +54,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Billboard;
+export default Billboard
