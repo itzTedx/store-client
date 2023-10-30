420e36c62c0c76f680d4e29d9f03af5590ce39fd
itzTedx <zomboi.offi@gmail.com>
2023-10-30 10:48:46 +0400

    Home page ui update

---
+++ app/(routes)/_components/Explore.tsx
@@ -0,0 +1,16 @@
+const Explore = () => {
+  return (
+    <div>
+      Start Exploring
+      <div className="h-auto w-px bg-neutral-200 hidden md:block" />
+      <ul>
+        <li>Business cards</li>
+        <li>Rollup Stand</li>
+        <li>Flyers</li>
+        <li>Stickers</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Explore;
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,25 +1,27 @@
-import Billboard from "@/components/Billboard"
-import Container from "@/components/ui/container"
-import getBillboard from "@/actions/get-billboard"
-import getProducts from "@/actions/get-products"
-import ProductList from "@/components/ProductList"
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/ProductList";
+import Explore from "./_components/Explore";
 
-export const revalidate = 30
+export const revalidate = 30;
 
 const HomePage = async () => {
-  const billboard = await getBillboard("90f88b2f-7192-4d66-ae65-9fc4170773d9")
-  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("90f88b2f-7192-4d66-ae65-9fc4170773d9");
+  const products = await getProducts({ isFeatured: true });
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <Explore />
         <div className="flex flex-col gap-y-8 sm:px-6 lg:px-8 px-4">
-          <ProductList title="Featured Products" items={products} />
+          <ProductList title="Featured Products" items={products} />E
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
