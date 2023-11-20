f3d4dcd550bacdf79c3a50d5397c8c511a10ed2f
melwin-tedd <musictrendiez@gmail.com>
2023-11-21 02:01:33 +0400

    orders page fix

--- app/(routes)/(users)/my-orders/page.tsx
+++ app/(routes)/(users)/my-orders/page.tsx
@@ -1,11 +1,8 @@
-import getOrders from '@/actions/get-orders'
+import getOrders from "@/actions/get-orders"
 
 const MyOrdersPage = async () => {
-  const orders = await getOrders()
-
   return (
     <div className="container my-12">
-      My Orders {orders.length}
       <div>items:</div>
     </div>
   )
