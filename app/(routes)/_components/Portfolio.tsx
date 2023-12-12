6e9a9a65062271fb367ed13439278cafe009add4
itzTedx <zomboi.offi@gmail.com>
2023-12-12 23:53:59 +0400

    Test for dynamic loading

--- app/(routes)/_components/Portfolio.tsx
+++ app/(routes)/_components/Portfolio.tsx
@@ -1,65 +1,65 @@
-"use client";
+'use client'
 
-import { useRef } from "react";
-import { useScroll, useTransform, motion } from "framer-motion";
-import Image from "next/image";
+import { useRef } from 'react'
+import { useScroll, useTransform, motion } from 'framer-motion'
+import Image from 'next/image'
 
 const slider1 = [
   {
-    color: "#e3e5e7",
-    src: "c2.jpg",
+    color: '#e3e5e7',
+    src: 'c2.jpg',
   },
   {
-    color: "#d6d7dc",
-    src: "decimal.jpg",
+    color: '#d6d7dc',
+    src: 'decimal.jpg',
   },
   {
-    color: "#e3e3e3",
-    src: "funny.jpg",
+    color: '#e3e3e3',
+    src: 'funny.jpg',
   },
   {
-    color: "#21242b",
-    src: "google.jpg",
+    color: '#21242b',
+    src: 'google.jpg',
   },
   {
-    color: "#e5e0e1",
-    src: "panda.jpg",
+    color: '#e5e0e1',
+    src: 'panda.jpg',
   },
-];
+]
 
 const slider2 = [
   {
-    color: "#d4e3ec",
-    src: "maven.jpg",
+    color: '#d4e3ec',
+    src: 'maven.jpg',
   },
   {
-    color: "#e5e0e1",
-    src: "panda.jpg",
+    color: '#e5e0e1',
+    src: 'panda.jpg',
   },
   {
-    color: "#d7d4cf",
-    src: "powell.jpg",
+    color: '#d7d4cf',
+    src: 'powell.jpg',
   },
   {
-    color: "#e1dad6",
-    src: "wix.jpg",
+    color: '#e1dad6',
+    src: 'wix.jpg',
   },
   {
-    color: "#e5e0e1",
-    src: "panda.jpg",
+    color: '#e5e0e1',
+    src: 'panda.jpg',
   },
-];
+]
 
-export default function index() {
-  const container = useRef(null);
+export default function Portfolio() {
+  const container = useRef(null)
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start end", "end start"],
-  });
+    offset: ['start end', 'end start'],
+  })
 
-  const x1 = useTransform(scrollYProgress, [0, 1], [0, 150]);
-  const x2 = useTransform(scrollYProgress, [0, 1], [0, -150]);
-  const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
+  const x1 = useTransform(scrollYProgress, [0, 1], [0, 150])
+  const x2 = useTransform(scrollYProgress, [0, 1], [0, -150])
+  const height = useTransform(scrollYProgress, [0, 0.9], [50, 0])
 
   return (
     <div ref={container} className="flex flex-col gap-3 relative my-9 z-[1]">
@@ -82,13 +82,13 @@ export default function index() {
               <div className="relative w-[80%] h-[80%]">
                 <Image
                   fill={true}
-                  alt={"image"}
+                  alt={'image'}
                   src={`/images/${project.src}`}
                   className="object-cover"
                 />
               </div>
             </div>
-          );
+          )
         })}
       </motion.div>
       <motion.div
@@ -105,17 +105,17 @@ export default function index() {
               <div key={index} className="relative w-[80%] h-[80%]">
                 <Image
                   fill
-                  alt={"image"}
+                  alt={'image'}
                   src={`/images/${project.src}`}
                   className="object-cover"
                 />
               </div>
             </div>
-          );
+          )
         })}
       </motion.div>
     </div>
-  );
+  )
 }
 
 // "use client";
