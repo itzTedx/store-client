6d493e42d20f123431ada00e2bc5cbd52c9d545c
itzTedx <zomboi.offi@gmail.com>
2023-12-21 12:33:28 +0400

    dev pwa disable

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  // disable: process.env.NODE_ENV === "development",
+  dest: "public/pwa",
+  disable: process.env.NODE_ENV === "development",
   // register: true,
   // scope: "/app",
   // sw: "service-worker.js",
