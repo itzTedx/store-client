230b1e4694ed91342e6ecc42944fade1ba0b231c
itzTedx <zomboi.offi@gmail.com>
2023-12-23 19:02:59 +0400

    Loader

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,16 +1,17 @@
-import Footer from '@/components/Footer'
-import BreakpointIndicator from '@/components/misc/BreakpointIndicator'
-import Navbar from '@/components/navbar/Navbar'
+import Footer from "@/components/Footer";
+import BreakpointIndicator from "@/components/misc/BreakpointIndicator";
+import Navbar from "@/components/navbar/Navbar";
 
-import BottomBar from '@/components/navbar/BottomNavBar'
-import ModalProvider from '@/providers/modal-provider'
-import { ThemeProvider } from '@/providers/theme-provider'
-import ToastProvider from '@/providers/toast-provider'
+import BottomBar from "@/components/navbar/BottomNavBar";
+import ModalProvider from "@/providers/modal-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
+import ToastProvider from "@/providers/toast-provider";
+import NextTopLoader from "nextjs-toploader";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ThemeProvider
@@ -19,6 +20,22 @@ export default function RootLayout({
       enableSystem
       storageKey="theme"
     >
+      <NextTopLoader
+        color="#2299DD"
+        initialPosition={0.08}
+        crawlSpeed={200}
+        height={3}
+        crawl={true}
+        showSpinner={false}
+        easing="ease"
+        speed={200}
+        shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        template='<div class="bar" role="bar">
+                    <div class="peg"></div>
+                  </div>'
+        zIndex={99999999999}
+        showAtBottom={false}
+      />
       <ToastProvider />
       <ModalProvider />
       {/* <WelcomeModal /> */}
@@ -29,5 +46,5 @@ export default function RootLayout({
       <Footer />
       <BottomBar />
     </ThemeProvider>
-  )
+  );
 }
